25b1db13e9b274ca36bc1868c3ac571f2b9f9c14
ipipman <ipipman@163.com>
2024-05-25 10:56:21 +0800

    add load balancer

--- src/main/java/cn/ipman/gateway/GatewayHandler.java
+++ src/main/java/cn/ipman/gateway/GatewayHandler.java
@@ -1,8 +1,11 @@
 package cn.ipman.gateway;
 
+import cn.ipman.rpc.core.api.LoadBalancer;
 import cn.ipman.rpc.core.api.RegistryCenter;
+import cn.ipman.rpc.core.cluster.RoundRibonLoadBalancer;
 import cn.ipman.rpc.core.meta.InstanceMeta;
 import cn.ipman.rpc.core.meta.ServiceMeta;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -25,6 +28,8 @@ public class GatewayHandler {
     @Autowired
     RegistryCenter rc;
 
+    LoadBalancer<InstanceMeta> loadBalancer = new RoundRibonLoadBalancer<>();
+
     Mono<ServerResponse> handler(ServerRequest request) {
         // 1. 通过请求路径获取服务名
         String service = request.path().substring(4);
@@ -39,30 +44,35 @@ public class GatewayHandler {
         // 2. 通过注册中心, 拿到所有有效的服务实例
         List<InstanceMeta> instanceMetas = rc.fetchAll(serviceMeta);
 
-        // 3. 先简化处理, 拿到第一个实例的url
-        String url = instanceMetas.get(0).toHttpUrl();
-        System.out.println(url);
+        // 3. 通过负载均衡, 拿到一个实例的url
+        System.out.println("instanceMetas -> " + instanceMetas);
+        InstanceMeta instanceMeta = loadBalancer.choose(instanceMetas);
+        String url = instanceMeta.toHttpUrl();
+        System.out.println("loadBalance to instanceMeta -> " + instanceMeta);
 
         // 4. 拿到请求的报文
         Mono<String> requestMono = request.bodyToMono(String.class);
-        return requestMono.flatMap(x -> {
-            // 5. 通过webclient 发送post请求
-            WebClient client = WebClient.create(url);
-            Mono<ResponseEntity<String>> entity = client.post()
-                    .header("Content-Type", "application/json")
-                    .bodyValue(x)
-                    .retrieve()
-                    .toEntity(String.class);
+        return requestMono.flatMap(x -> invokerFromRegistry(x, url));
+    }
 
-            // 6. 通过entity 获取响应报文
-            Mono<String> body = entity.mapNotNull(ResponseEntity::getBody);
-            body.subscribe(source -> System.out.println("response:" + source));
 
-            // 7. 组装响应报文
-            return ServerResponse.ok()
-                    .header("Content-Type", "application/json")
-                    .header("ipman.gw.version", "v1.0.0")
-                    .body(body, String.class);
-        });
+    private static @NotNull Mono<ServerResponse> invokerFromRegistry(String x, String url) {
+        // 5. 通过webclient 发送post请求
+        WebClient client = WebClient.create(url);
+        Mono<ResponseEntity<String>> entity = client.post()
+                .header("Content-Type", "application/json")
+                .bodyValue(x)
+                .retrieve()
+                .toEntity(String.class);
+
+        // 6. 通过entity 获取响应报文
+        Mono<String> body = entity.mapNotNull(ResponseEntity::getBody);
+        body.subscribe(source -> System.out.println("response:" + source));
+
+        // 7. 组装响应报文
+        return ServerResponse.ok()
+                .header("Content-Type", "application/json")
+                .header("ipman.gw.version", "v1.0.0")
+                .body(body, String.class);
     }
 }
