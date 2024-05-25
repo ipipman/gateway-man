98ed83fa147bbb7c80765dbb4b11ac9b5e710936
ipipman <ipipman@163.com>
2024-05-25 10:39:54 +0800

    fix webflux flatmap

--- src/main/java/cn/ipman/gateway/GatewayHandler.java
+++ src/main/java/cn/ipman/gateway/GatewayHandler.java
@@ -35,34 +35,34 @@ public class GatewayHandler {
                 .namespace("public")
                 .version("1.0")
                 .build();
+
         // 2. 通过注册中心, 拿到所有有效的服务实例
         List<InstanceMeta> instanceMetas = rc.fetchAll(serviceMeta);
+
         // 3. 先简化处理, 拿到第一个实例的url
         String url = instanceMetas.get(0).toHttpUrl();
         System.out.println(url);
 
         // 4. 拿到请求的报文
         Mono<String> requestMono = request.bodyToMono(String.class);
-        System.out.println(requestMono);
+        return requestMono.flatMap(x -> {
+            // 5. 通过webclient 发送post请求
+            WebClient client = WebClient.create(url);
+            Mono<ResponseEntity<String>> entity = client.post()
+                    .header("Content-Type", "application/json")
+                    .bodyValue(x)
+                    .retrieve()
+                    .toEntity(String.class);
 
-        // 5. 通过webclient 发送post请求
-        WebClient client = WebClient.create(url);
-        Mono<ResponseEntity<String>> entity = client.post()
-                .header("Content-Type", "application/json")
-                .body(requestMono, String.class)
-                .retrieve()
-                .toEntity(String.class);
+            // 6. 通过entity 获取响应报文
+            Mono<String> body = entity.mapNotNull(ResponseEntity::getBody);
+            body.subscribe(source -> System.out.println("response:" + source));
 
-
-        // 6. 通过entity 获取响应报文
-        Mono<String> body = entity.mapNotNull(ResponseEntity::getBody);
-        body.subscribe(source -> System.out.println("response:" + source));
-
-        // 7. 组装响应报文
-        return ServerResponse.ok()
-                .header("Content-Type", "application/json")
-                .header("ipman.gw.version", "v1.0.0")
-                .body(body, String.class);
-
+            // 7. 组装响应报文
+            return ServerResponse.ok()
+                    .header("Content-Type", "application/json")
+                    .header("ipman.gw.version", "v1.0.0")
+                    .body(body, String.class);
+        });
     }
 }
