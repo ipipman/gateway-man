89da501047a6c384ecafc7dc736dc4e5dcc81780
ipipman <ipipman@163.com>
2024-05-25 08:54:20 +0800

    add web client

--- src/main/java/cn/ipman/gateway/HelloHandler.java
+++ src/main/java/cn/ipman/gateway/HelloHandler.java
@@ -1,6 +1,8 @@
 package cn.ipman.gateway;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
@@ -14,7 +16,24 @@ import reactor.core.publisher.Mono;
 @Component
 public class HelloHandler {
 
-    Mono<ServerResponse> handler(ServerRequest request) {
-        return ServerResponse.ok().bodyValue("Hello, Spring WebFlux");
+    Mono<ServerResponse> handler(@SuppressWarnings("unused") ServerRequest request) {
+
+        String url = "http://192.168.31.232:9081/rpcman";
+        String requestJson = """
+                {
+                  "service": "cn.ipman.rpc.demo.api.UserService",
+                  "methodSign": "findById@1_int",
+                  "args": [100]
+                }
+                """;
+
+        WebClient client = WebClient.create(url);
+        Mono<ResponseEntity<String>> entity = client.post()
+                .header("Content-Type", "application/json")
+                .bodyValue(requestJson)
+                .retrieve()
+                .toEntity(String.class);
+
+        return ServerResponse.ok().body(entity.mapNotNull(ResponseEntity::getBody), String.class);
     }
 }
